fca141035ffe3f3098ca219f7181c4133ad2d267
Scott Baker <scott.baker@directlink.coop>
2022-04-14 10:43:19 -0700

    Make the tests cleaner and output a total output

--- tests.php
+++ tests.php
@@ -8,6 +8,17 @@ $sluz = new sluz;
 $sluz->debug = 0;
 $sluz->in_unit_test = 1;
 
+// Test counters
+$pass_count = 0;
+$fail_count = 0;
+// ANSI colors and strings for testing output
+$green    = "\033[38;5;10m";
+$red      = "\033[38;5;9m";
+$reset    = "\033[0m";
+$white    = "\033[38;5;15m";
+$ok_str   = $white . "[" . $green . "  OK  " . $reset . $white . "]" . $reset;
+$fail_str = $white . "[" . $red   . " FAIL " . $reset . $white . "]" . $reset;
+
 $sluz->assign('key'    , 'val');
 $sluz->assign('first'  , "Scott");
 $sluz->assign('last'   , "Baker");
@@ -20,6 +31,10 @@ $sluz->assign('number' , 15);
 $sluz->assign('members', [['first' => 'Scott', 'last' => 'Baker'], ['first' => 'Jason', 'last' => 'Doolis']]);
 $sluz->assign('subarr' , ['one' => [2,4,6], 'two' => [3,6,9]]);
 $sluz->assign('arrayd' , [[1,2],[3,4],[5,6]]);
+
+if ($_SERVER['SERVER_SOFTWARE'] === "Apache") {
+	kd($sluz->process_block('{foreach $subarr.one as $id}{$id}{/foreach}'));
+}
 
 sluz_test('Hello there'         , 'Hello there', 'Basic #1');
 sluz_test('{$first}'            , 'Scott'      , 'Basic #2');
@@ -59,7 +74,7 @@ sluz_test('<div>Scott</div>', '<div>Scott</div>', 'Plain text #2 - HTML');
 
 // Don't parse blocks that have whitespacing
 sluz_test(' {$first} ', ' {$first} ', 'Bad block #1');
-sluz_test('{first}'   , '{first}'   , 'Bad block #2'); // Literal (no $)
+sluz_test('{first}'   , NULL        , 'Bad block #2'); // Literal (no $)
 
 sluz_test('{literal}{{/literal}'                  , '{'                  , 'Literal #1');
 sluz_test('{literal}}{/literal}'                  , '}'                  , 'Literal #2');
@@ -67,10 +82,26 @@ sluz_test('{literal}{}{/literal}'                 , '{}'                 , 'Lite
 sluz_test('{literal}{literal}{/literal}'          , '{literal}'          , 'Literal #4');
 sluz_test('{literal}{literal}{/literal}{/literal}', '{literal}{/literal}', 'Literal #5 - meta literal');
 
+$total = $pass_count + $fail_count;
+
+print "\n";
+if ($fail_count === 0) {
+	$msg = sprintf("All %d tests passed successfully", $total);
+	print $msg . str_repeat(" ", 81 - strlen($msg)) . $ok_str . "\n";
+} else {
+	$msg = sprintf("%d tests failed (%.1f%% failure rate)", $fail_count, ($fail_count / $total) * 100);
+	print $msg . str_repeat(" ", 81 - strlen($msg)) . $fail_str . "\n";
+}
+
 ////////////////////////////////////////////////////////
 
 function sluz_test($input, $expected, $test_name) {
 	global $sluz;
+	global $pass_count;
+	global $fail_count;
+	global $ok_str;
+	global $fail_str;
+
 	$html = $sluz->process_block($input);
 
 	$lead = "Test '$test_name' ";
@@ -81,17 +112,24 @@ function sluz_test($input, $expected, $test_name) {
 	$ok    = "\033[32m";
 	$fail  = "\033[31m";
 	$reset = "\033[0m";
+	$white = "\033[1m";
+
+	$l = $white . "[" . $reset;
+	$r = $white . "]" . $reset;
 
 	$expected = var_export($expected, true);
 	$html     = var_export($html, true);
 
 	if ($html === $expected) {
-		print $ok . "OK" . $reset . "\n";
+		print $ok_str . "\n";
+		$pass_count++;
 	} else {
 		$d = debug_backtrace();
 		$file = $d[0]['file'];
 		$line = $d[0]['line'];
-		print $fail . "FAIL" . $reset . "\n";
+		print $fail_str . "\n";
 		print "  * Expected $expected but got $html (from: $file #$line)\n";
+
+		$fail_count++;
 	}
 }
