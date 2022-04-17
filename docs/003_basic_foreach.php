dbdd932c970982e941e32f0051d9d24a80c60269
Scott Baker <scott.baker@directlink.coop>
2022-04-16 22:40:38 -0700

    Add more foreach stuff

--- docs/003_basic_foreach.php
+++ docs/003_basic_foreach.php
@@ -3,6 +3,7 @@
 include("../sluz.class.php");
 $s = new sluz();
 
-$s->assign("turtles", ["Michelangeo", "Donatello", "Leonardo", "Raphael"]);
+$s->assign("turtles", ["Michelangeo", "Donatello", "Leonardo", "Raphael"]); // Array
+$s->assign("numbers", ["one" => "uno", "two" => "dos", "three" => "tres"]); // Hash
 
 print $s->parse();
