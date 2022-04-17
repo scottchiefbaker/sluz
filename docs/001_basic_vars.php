b9371e15698932f598a4f42352273fa824cd1f61
Scott Baker <scott.baker@directlink.coop>
2022-04-16 22:29:24 -0700

    Update the basic_vars example

--- docs/001_basic_vars.php
+++ docs/001_basic_vars.php
@@ -3,7 +3,10 @@
 include("../sluz.class.php");
 $s = new sluz();
 
-$s->assign("first", "Jason");
-$s->assign("last", "Doolis");
+$s->assign("first", "Jason"); // Scalar syntax
+$s->assign("last" , "Doolis");
+
+$s->assign("month", ["Jan", "Feb", "Mar"]); // Array syntax
+$s->assign("data" , ["color" => "red"]);    // Hash syntax
 
 print $s->parse();
