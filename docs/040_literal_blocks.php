7635185458d5be93e8b4e86e185da7a17a87f84b
Scott Baker <scott@perturb.org>
2022-04-21 13:50:14 -0700

    Update the documentation

--- docs/040_literal_blocks.php
+++ docs/040_literal_blocks.php
@@ -1,5 +1,12 @@
 <?php
 
+///////////////////////////////////////////////////////////////////////////////
+// You may need a literal '{' or '}' in your HTML. These characters are      //
+// common in both CSS and Javascript. To prevent Sluz from interpretting     //
+// them as code block just use {literal}.                                    //
+// Note: Brackets with whitespace on both sides do NOT require {literal}     //
+///////////////////////////////////////////////////////////////////////////////
+
 include("../sluz.class.php");
 $s = new sluz();
 
