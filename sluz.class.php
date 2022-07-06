50d214fbfc6a28c2ea82812b819f592eeaf0af5f
Scott Baker <scott@perturb.org>
2022-07-06 10:27:13 -0700

    Bump the version so we know we're working on newer code

--- sluz.class.php
+++ sluz.class.php
@@ -3,7 +3,7 @@
 ////////////////////////////////////////////////////////
 
 class sluz {
-	public $version      = '0.5';
+	public $version      = '0.6';
 	public $tpl_file     = null;
 	public $debug        = 0;
 	public $in_unit_test = false;
