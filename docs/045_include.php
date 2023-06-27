88668208afb394f9e8527b76235d9822bdcdad06
Scott Baker <scott@perturb.org>
2023-06-27 10:05:11 -0700

    Update include docs about relative paths

--- docs/045_include.php
+++ docs/045_include.php
@@ -3,7 +3,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 // You can include external templates in your .stpl files. This allows you   //
 // to have a common header, footer, or framework. Included files have all    //
-// the same properties as normal templates                                   //
+// the same properties as normal templates. Templates in {include} tags      //
+// will need to be pathed as *relative* to the including tpl.                //
 ///////////////////////////////////////////////////////////////////////////////
 
 include("../sluz.class.php");
