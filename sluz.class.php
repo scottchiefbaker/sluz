83bb34ceface920dbec444599281bd0291d9aa98
Scott Baker <scott@perturb.org>
2023-07-20 09:13:14 -0700

    Single char compare is SLIGHLY (2%) faster than str_starts_with()

--- sluz.class.php
+++ sluz.class.php
@@ -49,7 +49,7 @@ class sluz {
 		}
 
 		// If it doesn't start with a '{' it's plain text so we just return it
-		if (!str_starts_with($str, '{')) {
+		if ($str[0] !== '{') {
 			$ret = $str;
 		// Simple variable replacement {$foo} or {$foo|default:"123"}
 		} elseif (str_starts_with($str, '{$') && preg_match('/^\{\$(\w[\w\|\.\'":,]*)\s*\}$/', $str, $m)) {
