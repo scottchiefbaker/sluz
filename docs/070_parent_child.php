2cb516de8f4449b0a925f48b148acb0fa42d9464
Scott Baker <scott@perturb.org>
2024-06-29 19:17:28 -0700

    Docs for parent templates

---
+++ docs/070_parent_child.php
@@ -0,0 +1,19 @@
+<?php
+
+////////////////////////////////////////////////////////////////////////////
+// It's common to have a global header/footer and include content in the  //
+// middle. This can be accomplished with a parent template. When a parent //
+// template is specified $__CHILD_TPL is populated with the original      //
+// template file. This can then be {include file="$__CHILD_TPL"} in the   //
+// parent.                                                                //
+////////////////////////////////////////////////////////////////////////////
+
+require('../sluz.class.php');
+$s = new sluz();
+
+// Two parameter fetch()
+print $s->fetch("tpls/070_child.stpl", "tpls/070_parent.stpl");
+
+// You can also set a parent template and then use normal fetch()
+//$s->parent_tpl("tpls/070_parent.stpl");
+//print $s->fetch("tpls/070_child.stpl");
