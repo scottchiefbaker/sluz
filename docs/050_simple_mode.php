2a69172b9def68320c806e05af6c6601b3460f08
Scott Baker <scott@perturb.org>
2025-09-10 14:02:40 -0700

    Update the simple mode docs

--- docs/050_simple_mode.php
+++ docs/050_simple_mode.php
@@ -13,5 +13,8 @@ $s = sluz("weather", "Sure is sunny today");
 $s = sluz("temperature", 75);
 
 // Note: fetch() is automatically called in simple mode as part of the destructor.
+// Template file used is the PHP filename with the .php changed to .stpl in
+// the tpls/ directory.
+//
 // If you need an alternate template you have to call fetch() manually like:
 // print $s->fetch("tpls/special.stpl");
