bade9b5a334c90a91e99cea68ad153a49d081823
Scott Baker <scott@perturb.org>
2026-07-09 11:11:50 -0700

    Accept JSON or YAML in the sandbox

--- docs/sandbox/index.php
+++ docs/sandbox/index.php
@@ -6,11 +6,27 @@
 	$s = new sluz();
 
 	if ($json && $tpl) {
-		$obj = json_decode($json, true);
+		$obj  = json_decode($json, true);
+		$type = 'json';
+
+		if ($obj === null && json_last_error() !== JSON_ERROR_NONE) {
+			$obj = yaml_parse($json);
+
+			if (is_array($obj)) {
+				$type = 'yaml';
+			} else {
+				$type = false;
+			}
+		}
+
 		$s->assign($obj);
 
 		$parsed = $s->parse_string($tpl);
-		$ret    = ['parsed' => $parsed];
+		$ret    = [
+			'type'   => $type,
+			'parsed' => $parsed,
+			'raw'    => $obj,
+		];
 
 		print json_encode($ret);
 		exit;
@@ -27,7 +43,19 @@
 
 		<script type="text/javascript" src="../js/jquery.min.js"></script>
 		<script>
-			var json_sample = '{"true":true,"nums":[1,2,3,4,5],"name":"Jason Doolis","customer":{"name":"Thomas","age":39,"color":"purple"},"fruits":[["Apple","Banana","Cherry"],["Pear","Orange","Grape"],["Lime","Lemon","Mango"]],"orders":[{"ID":"1","Total":"$99","Items":"3"},{"ID":"2","Total":"$299","Items":"6"},{"ID":"3","Total":"$50","Items":"10"},{"ID":"4","Total":"$75","Items":"27"},{"ID":"5","Total":"$200","Items":"4"}]}';
+			var json_sample = `---
+name: Jason Doolis
+color: Red
+orders:
+- ID: "1"
+  Total: $99
+  Items: "3"
+- ID: "2"
+  Total: $299
+  Items: "6"
+- ID: "3"
+  Total: $50
+  Items: "10"`;
 			var sluz_tpl = '<h2>Orders</h2>\n\n{foreach $orders as $x}\n<div>{$x.ID}) Total: {$x.Total} in items: {$x.Items}</div>\n{/foreach}\n\n<p class=\"mt-3\">Customer: {$name}</p>';
 
 			$(document).ready(function() {
@@ -65,20 +93,8 @@
 			function process() {
 				var tpl  = $("#sluz_input").val();
 				var json = $("#json_input").val();
-				var data = {};
 
 				var bad_color = 'rgb(46, 24, 28)';
-
-				try {
-					if (!json) { return; }
-
-					data = JSON.parse(json);
-
-					$("#json_input").css('background', 'inherit');
-				} catch {
-					console.log('bad json');
-					$("#json_input").css('background', bad_color);
-				}
 
 				try {
 					var data = { 'json': json, 'tpl': tpl, };
@@ -89,14 +105,24 @@
 						data    : data,
 						success : function(e) {
 							var out_text = e.parsed;
+							var ok       = (e.type !== false);
+
+							if (!ok) {
+								console.log('Unknown input');
+								$("#json_input").css('background', bad_color);
+							} else {
+								$("#json_input").css('background', 'inherit');
+							}
 
 							$("#sluz_text").val(out_text);
 							$("#html_output").html(out_text);
 							$("#sluz_input").css('background', 'inherit');
 						},
 						error : function(e) {
-							console.log('bad sluz');
-							$("#sluz_input").css('background', bad_color);
+							if (tpl) {
+								console.log('bad sluz');
+								$("#sluz_input").css('background', bad_color);
+							}
 						}
 					});
 
@@ -124,8 +150,8 @@
 		<div class="col">
 			<div class="p-2">
 				<div class="mb-3">
-					JSON Input:
-					<textarea id="json_input" class="w-100" rows="9" placeholder="JSON input"></textarea>
+					JSON/YAML Input:
+					<textarea id="json_input" class="w-100" rows="9" placeholder="JSON/YAML input"></textarea>
 				</div>
 
 				<div class="mb-3">
